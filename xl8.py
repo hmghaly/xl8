e42bb1fc637d4394e6664b5cfa744ef48b1e4081
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-09-25 15:18:53 -0400

    Update xl8.py

--- xl8.py
+++ xl8.py
@@ -1,5 +1,25 @@
 from flask import Flask, redirect, url_for, request,render_template, send_file, send_from_directory
 from flask import jsonify
+import os, sys, json, time, re, copy
+
+app = Flask(__name__)
+
+#app.wsgi_app = ProxyFix(app.wsgi_app)
+
+@app.route('/assets/<path:path>')
+def serve_file(path):
+    return send_from_directory('assets', path)  
+
+
+@app.route('/')
+def home_page():
+  out_dict={}
+  out_dict["message"]="success"
+  return json.dumps(out_dict)
+
+  #index_file_dom=DOM(read_file("index.html"))
+  #home_repl_dict={".page":""}
+
 
 if __name__ == '__main__':
   from waitress import serve
