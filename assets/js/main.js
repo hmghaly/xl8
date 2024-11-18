8d8325d2a86e2043108a6988a696fb2b7a1dc14b
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-11-18 06:02:17 -0500

    bitext table

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,13 @@ async function submit_analyze(){
   console.log(res)
   $$("output_div").hidden=false;
   $$("input_div").hidden=true;
+  //populate table
+  cur_table=$$("bitext_table")
+  align_output=res["align_output"] || {}
+  aligned_pairs=align_output["alignment"] || []
+  for (it0 of aligned_pairs){
+    console.log(it0)
+  }
 
 }
 
