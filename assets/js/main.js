615e962933ea6df434a89e0edad205f6723cd2b5
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-11-18 05:52:14 -0500

    input/output navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,14 @@ async function submit_analyze(){
   console.log(lang_form_dict)	
   res=await post_data_async("align_api",lang_form_dict)
   console.log(res)
+  $$("output_div").hidden=false;
+  $$("input_div").hidden=true;
+
+}
+
+
+function back2input(){
+  $$("output_div").hidden=true;
+  $$("input_div").hidden=false;
 
 }
