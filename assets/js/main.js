a76c4fa2a829e523bd105b1aca4c626e2dc681f2
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-11-18 05:28:54 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,6 @@ async function submit_analyze(){
   lang_form_dict= await get_form_vals("lang_input_form")
   console.log(lang_form_dict)	
   res=await post_data_async("align_api",lang_form_dict)
+  console.log(res)
 
 }
