122a2d8d1cb9db125dfbdf31cea7afcf6efd22c8
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-11-18 06:04:51 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,16 @@ async function submit_analyze(){
   aligned_pairs=align_output["alignment"] || []
   for (it0 of aligned_pairs){
     console.log(it0)
+    var row = cur_table.insertRow(-1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+
   }
 
 }
